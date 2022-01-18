c26c869680e0d407cc0b72281c3f9c212d14d11f
Ray Orolé <RayO512@LLN.CampusSintUrsula.be>
2022-01-18 14:43:14 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { EyeIcon, GlobeAltIcon, ScaleIcon, LightningBoltIcon, AnnotationIcon } from "@heroicons/react/outline";
+import "./App.css";
 
 function App() {
+
+  const features = [
+    {
+      name: 'Competitive exchange rates',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: GlobeAltIcon,
+    },
+    {
+      name: 'No hidden fees',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: ScaleIcon,
+    },
+    {
+      name: 'Transfers are instant',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: LightningBoltIcon,
+    },
+    {
+      name: 'Mobile notifications',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: AnnotationIcon,
+    },
+  ]
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="bg-slate-600">
+      <header className="p-3 bg-indigo-700 flex text-white justify-between items-center">
+        <h2 className="text-xl font-semibold">Gok Spelletje</h2>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/rayorole/randomgame-school"
           target="_blank"
-          rel="noopener noreferrer"
+          className="px-3 py-1 flex justify-center items-center text-sm font-semibold bg-white text-indigo-500 hover:text-white hover:bg-indigo-500 hover:rounded-xl hover:animate-none transition duration-300 ease-in-out rounded-lg"
         >
-          Learn React
+          Bekijk code
+          <EyeIcon className="h-5 w-5 ml-2" />
         </a>
       </header>
+      <main className="bg-neutral-100 px-24 flex justify-center text-center">
+        <div className="py-24">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="lg:text-center">
+              <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+                Gok Spelletje
+              </h2>
+              <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                Gemaakt door Ray Orolé
+              </p>
+              <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
+                magnam voluptatum cupiditate veritatis in accusamus quisquam.
+              </p>
+            </div>
+
+            <div className="mt-10">
+              <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative">
+                    <dt>
+                      <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                        <feature.icon className="h-6 w-6" aria-hidden="true" />
+                      </div>
+                      <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                        {feature.name}
+                      </p>
+                    </dt>
+                    <dd className="mt-2 ml-16 text-base text-gray-500">
+                      {feature.description}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
